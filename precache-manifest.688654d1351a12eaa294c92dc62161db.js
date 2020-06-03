c9da4e77b327821f6f684ab541b94017c6368255
Marv Odemwingie <odemwingiemarv@gmail.com>
2020-06-03 18:53:22 +0100

    Updates

---
+++ precache-manifest.688654d1351a12eaa294c92dc62161db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59f4367eb0b5a02b2e78da7fdbf0349",
+    "url": "/basic-expense-tracker/index.html"
+  },
+  {
+    "revision": "995bae6b0680cf038ac6",
+    "url": "/basic-expense-tracker/static/css/main.f77dfa86.chunk.css"
+  },
+  {
+    "revision": "52e413b76c43293c7e9e",
+    "url": "/basic-expense-tracker/static/js/2.650c0dfb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/basic-expense-tracker/static/js/2.650c0dfb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "995bae6b0680cf038ac6",
+    "url": "/basic-expense-tracker/static/js/main.6d750eac.chunk.js"
+  },
+  {
+    "revision": "4474fd3b0f5864a5f810",
+    "url": "/basic-expense-tracker/static/js/runtime-main.1acec2b2.js"
+  }
+]);
